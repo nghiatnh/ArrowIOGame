971323ea8a00adb894e00aa54a3cfece544ab910
nghiatnh <nghiatnh02@gmail.com>
2021-12-21 01:23:38 +0700

    Add Skills Sprites and class SkillController.cs

---
+++ Assets/Scripts/SkillController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillController
+{
+    
+}
+
+public enum SKILLS{
+    MAGNET,
+    RANGE_UP,
+    CRIT,
+    BLOOD_SUCKING,
+    ALL_STATUS_UP,
+    THROUGH_WALL,
+    MORE_EXP,
+    ATK_UP,
+    ATK_UP_HP_DOWN,
+    DEF_UP,
+    HP_UP,
+    SPEED_UP,
+    BULLET_UP,
+    KILL_HP_UP,
+    BULLET_THROUGH_WALL,
+    MAKE_SPIKE,
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    public Sprite[] Skills;
+    public Transform pnSkill;
+    public bool isRandom = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isRandom)
+            RandomSkill(isRandom);
+    }
+
+    void RandomSkill(bool isRandom){
+        List<int> ListSkill = new List<int>();
+        for (int i = 0; i < Enum.GetValues(typeof(SKILLS)).Length; i++){
+            ListSkill.Add(i);
+        }
+        if (isRandom)
+            for (int i = 0; i < pnSkill.childCount; i++){
+                Transform skill = pnSkill.GetChild(i);
+                int index = UnityEngine.Random.Range(0, ListSkill.Count);
+                skill.GetComponent<Image>().sprite = Skills[ListSkill[index]];
+                ListSkill.RemoveAt(index);
+            }
     }
 }
